a0552981f551b5cc933bedf6bb810612ed2edb04
frolmaslov <frol_mv@mail.ru>
2022-11-25 15:06:19 +0100

    Add project

--- cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('detail', views.detail),
-    path('add', views.cart_add, name='cart_add'),
-    path('remove', views.cart_remove, name='cart_remove'),
-
-]
